299f45d2c02cc2f0b3eea1878d69ec2108eaaabd
FilArt <artfil@protonmail.com>
2020-06-11 01:29:11 +0200

    remove footer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -47,9 +47,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
