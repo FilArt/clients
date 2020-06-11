e64045295474d7834c3075cb2decaa59f8033f44
FilArt <artfil@protonmail.com>
2020-06-11 16:36:33 +0200

    update bids

--- apps/bids/models.py
+++ apps/bids/models.py
@@ -1,8 +1,32 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+def more_than_zero(value):
+    if value <= 0:
+        raise ValidationError(_("Ensure this value is greater than 0."))
+
+
+class PositiveNullableFloatField(models.FloatField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.blank = self.null = True
+        self.validators.append(validators.MinValueValidator(0))
 
 
 class Bid(models.Model):
-    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
-    offer = models.OneToOneField('calculator.Offer', on_delete=models.CASCADE)
+    user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
+    offer = models.ForeignKey("calculator.Offer", on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # показатели объекта (дом, кафе и тд)
+    c1 = PositiveNullableFloatField(validators=[more_than_zero])
+    c2 = PositiveNullableFloatField()
+    c3 = PositiveNullableFloatField()
+    p1 = PositiveNullableFloatField(validators=[more_than_zero])
+    p2 = PositiveNullableFloatField()
+    p3 = PositiveNullableFloatField()
+    annual_consumption = PositiveNullableFloatField()
--- apps/bids/viewsets.py
+++ apps/bids/viewsets.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from .models import Bid
 from .serializers import BidSerializer, BidListSerializer, BidWithOferta
@@ -6,7 +9,7 @@ from .serializers import BidSerializer, BidListSerializer, BidWithOferta
 
 class BidViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
-        if self.action in ('create',):
+        if self.action in ("create",):
             return BidSerializer
         elif self.detail:
             return BidWithOferta
@@ -17,3 +20,12 @@ class BidViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=False)
+    def from_calculator(self, request: Request):
+        serializer = BidSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        bid_serializer = BidWithOferta(data=serializer.validated_data)
+        bid_serializer.is_valid(raise_exception=True)
+        bid_serializer.save(user=request.user)
+        return Response(bid_serializer.data)
