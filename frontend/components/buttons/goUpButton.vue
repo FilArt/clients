cb0094c3c80c0477009706994e61e573494feb71
FilArt <artfil@protonmail.com>
2020-06-14 00:43:34 +0200

    add goUpButton.vue

---
+++ frontend/components/buttons/goUpButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    fab
+    x-large
+    icon
+    large
+    fixed
+    bottom
+    right
+    color="yellow"
+    @click="$vuetify.goTo(0)"
+  >
+    <v-icon>mdi-arrow-up</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'goUpButton',
+}
+</script>
+
+<style scoped></style>
