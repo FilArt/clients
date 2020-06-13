5d45209801b36dd28e14e53e4ba4c1e1005beb52
FilArt <artfil@protonmail.com>
2020-06-14 00:44:24 +0200

    add StatusSelect.vue

---
+++ frontend/components/selects/StatusSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-autocomplete
+    v-model="item"
+    :items="statuses"
+    :multiple="multiple"
+    label="Estados"
+    style="min-width: 150px;"
+    chips
+    dense
+    deletable-chips
+    @input="$emit('input', status)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'StatusSelect',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    multiple: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      item: this.value,
+      statuses: [],
+    }
+  },
+  async mounted() {
+    this.statuses = await this.$axios.$get('bids/statuses')
+  },
+  watch: {
+    value: {
+      handler: function (val) {
+        this.item = val
+      },
+      deep: true,
+    },
+  },
+}
+</script>
