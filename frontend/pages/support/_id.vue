28a5baba0639c9036031ccf612055ec05b8a843b
FilArt <artfil@protonmail.com>
2020-06-16 17:07:29 +0200

    refactor support view

--- frontend/pages/support/_id.vue
+++ frontend/pages/support/_id.vue
@@ -1,50 +1,5 @@
 <template>
   <v-card>
-    <v-dialog v-model="submitDialog">
-      <template v-slot:activator="{ on }">
-        <v-btn
-          v-on="on"
-          fab
-          x-large
-          icon
-          large
-          fixed
-          bottom
-          right
-          color="success"
-        >
-          <v-icon>mdi-check</v-icon>
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title
-          ><p class="flex-grow-1">Submit</p>
-          <close-button @click="submitDialog = false" />
-        </v-card-title>
-        <v-card-text>
-          <v-form @submit.prevent="submit" novalidate>
-            <status-select
-              v-model="data.status"
-              :errors="errorMessages.status"
-            />
-            <v-textarea v-model="data.message" label="Message" />
-
-            <v-card-actions>
-              <v-btn
-                block
-                type="submit"
-                color="success"
-                :disabled="!data.status || !data.message"
-                >Listo
-                <v-icon right>mdi-check</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-
     <v-card-title>Offer</v-card-title>
     <v-card-text>
       <detail-offer :offer="bid.offer" />
@@ -68,6 +23,51 @@
         Attachment {{ attachment.id }}
       </v-chip>
     </v-card-text>
+
+    <v-card-actions>
+      <v-dialog v-model="submitDialog">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" v-on="on" block
+            >Submit
+            <v-icon right>mdi-check</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title
+            ><p class="flex-grow-1">Submit</p>
+            <close-button @click="submitDialog = false" />
+          </v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="submit" novalidate>
+              <status-select
+                v-model="data.status"
+                :errors="errorMessages.status"
+              />
+              <v-textarea
+                v-model="data.message"
+                label="Message"
+                :placeholder="
+                  data.status === 'success'
+                    ? 'Contrato celebrado!'
+                    : 'No puede hacer un contrato porque...'
+                "
+              />
+
+              <v-card-actions>
+                <v-btn
+                  block
+                  type="submit"
+                  color="success"
+                  :disabled="!data.status || !data.message"
+                  >Listo
+                  <v-icon right>mdi-check</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -76,8 +76,15 @@ import DetailOffer from '~/components/detailOffer'
 import CardDetail from '~/components/CardDetail'
 import StatusSelect from '~/components/selects/StatusSelect'
 import CloseButton from '~/components/buttons/closeButton'
+import SubmitButton from '~/components/buttons/submitButton'
 export default {
-  components: { CloseButton, StatusSelect, CardDetail, DetailOffer },
+  components: {
+    SubmitButton,
+    CloseButton,
+    StatusSelect,
+    CardDetail,
+    DetailOffer,
+  },
   async asyncData({ $axios, params }) {
     const bid = await $axios.$get(`bids/${params.id}`)
     return {
