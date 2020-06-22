74773a9c0a1d5dcfc203be268969c320f2476421
FilArt <artfil@protonmail.com>
2020-06-22 13:45:30 +0200

    fix middleware

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -1,5 +1,5 @@
 export default function ({ $auth, redirect, route }) {
-  if (!$auth.loggedIn && route.path !== '/login') {
+  if (!$auth.loggedIn && route.name !== 'login') {
     return redirect('/login')
   }
 }
