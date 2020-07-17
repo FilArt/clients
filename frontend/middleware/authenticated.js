bbcce84af9802cf4bbe0180a26fbab58b885736c
FilArt <artfil@protonmail.com>
2020-07-17 19:21:23 +0200

    protect internal pages

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -2,4 +2,11 @@ export default function ({ $auth, redirect, route }) {
   if (!$auth.loggedIn && route.name !== 'login') {
     return redirect('/login')
   }
+  if (
+    (route.name.startsWith('admin') || route.name.startsWith('support')) &&
+    (!$auth.user.role !== 'admin' ||
+      !$auth.user.permissions.includes('support'))
+  ) {
+    return redirect('/profile')
+  }
 }
