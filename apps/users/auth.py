81ba63db3732ef8268b2d9bb9d7e31ed5da2bbc6
FilArt <artfil@protonmail.com>
2020-08-10 02:58:40 +0200

    update last login

---
+++ apps/users/auth.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import update_last_login
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+class ObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        result = super().post(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid()
+        update_last_login(None, serializer.user)
+        return result
+
+
+class RefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        result = super().post(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid()
+        update_last_login(None, serializer.user)
+        return result
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .auth import ObtainPairView, RefreshView
 from .routers import user_router
 from .views import get_me
 
 urlpatterns = [
     path("", include(user_router.urls)),
-    path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login", ObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh", RefreshView.as_view(), name="token_refresh"),
     path("me", get_me, name="me"),
 ]
