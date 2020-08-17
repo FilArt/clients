bbda7e1290c7e1c57e76e7f34ba6412ab8d3917f
FilArt <artfil@protonmail.com>
2020-08-17 15:47:00 +0200

    nice admin header

--- frontend/components/admin/AdminHeader.vue
+++ frontend/components/admin/AdminHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <v-btn nuxt to="/admin/users?clients=true" exact>
-      Clientes
-    </v-btn>
+  <v-btn-toggle>
     <v-btn nuxt to="/admin/users?leeds=true" exact>
       Leeds
     </v-btn>
     <v-btn nuxt to="/support">
       Tramitacion
     </v-btn>
-  </div>
+    <v-btn nuxt to="/admin/users?clients=true" exact>
+      Clientes
+    </v-btn>
+  </v-btn-toggle>
 </template>
 <script>
 export default {
