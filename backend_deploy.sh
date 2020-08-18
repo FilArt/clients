fb1e82ce4823a36bebae18e3e91fa68882b98791
FilArt <artfil@protonmail.com>
2020-08-19 00:14:07 +0200

    fix backend_deploy.sh

--- backend_deploy.sh
+++ backend_deploy.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-ssh cv \
+ssh cv "\
   cd /srv/http/clients; \
   git pull; \
-  poetry shell; \
-  poetry update; \
+  poetry install --no-dev --no-root; \
   ./manage.py migrate; \
   ./manage.py compilemessages -l es &> /dev/null; \
   ./manage.py collectstatic --noinput; \
-   supervisorctl restart clients:
+   supervisorctl restart clients:"
