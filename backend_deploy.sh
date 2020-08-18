7fb66245d88f25c669cb539505d6fc7101ffe81e
FilArt <artfil@protonmail.com>
2020-08-19 00:11:01 +0200

    some

--- backend_deploy.sh
+++ backend_deploy.sh
@@ -6,6 +6,7 @@ ssh cv \
   cd /srv/http/clients; \
   git pull; \
   poetry shell; \
+  poetry update; \
   ./manage.py migrate; \
   ./manage.py compilemessages -l es &> /dev/null; \
   ./manage.py collectstatic --noinput; \
