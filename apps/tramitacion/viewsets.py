83d537dd37723c345853eb5ad9373d5941fff8ac
FilArt <artfil@protonmail.com>
2020-08-23 23:46:14 +0200

    fix permissions bug

--- apps/tramitacion/viewsets.py
+++ apps/tramitacion/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Tramitacion
 from .permissions import TramitacionPermission
@@ -8,4 +9,4 @@ from .serializers import TramitacionSerializer
 class TramitacionViewSet(viewsets.ModelViewSet):
     queryset = Tramitacion.objects.all()
     serializer_class = TramitacionSerializer
-    permission_classes = [TramitacionPermission]
+    permission_classes = [IsAuthenticated, TramitacionPermission]
