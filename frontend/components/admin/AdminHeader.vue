697a7998c2455ce230f2708d8f54ad196387ad88
FilArt <artfil@protonmail.com>
2020-08-25 21:13:56 +0200

    refactor AdminHeader.vue

--- frontend/components/admin/AdminHeader.vue
+++ frontend/components/admin/AdminHeader.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-btn-toggle>
-    <v-btn nuxt to="/admin/users" exact>
-      Usuarios
-    </v-btn>
-    <v-btn nuxt to="/support">
-      Tramitacion
-    </v-btn>
-    <v-btn nuxt to="/admin/charts">
-      Charts
-    </v-btn>
-  </v-btn-toggle>
+  <div class="d-flex flex-wrap justify-space-around align-center">
+    <v-btn v-for="btn in buttons" :key="btn.to" color="#004680" dark :to="btn.to" nuxt v-text="btn.text" />
+  </div>
 </template>
 <script>
 export default {
   name: 'AdminHeader',
+  data() {
+    return {
+      buttons: [
+        { text: 'Dashboard', to: '/admin/dashboard' },
+        { text: 'Leeds', to: '/admin/leeds' },
+        { text: 'Clientes', to: '/admin/clientes' },
+        { text: 'Usuarios', to: '/admin/usuarios' },
+        { text: 'Tramitacion', to: '/support' },
+      ],
+    }
+  },
 }
 </script>
