b363c7cfedb848edf25e7e1f31714e8adf5692b2
FilArt <artfil@protonmail.com>
2020-09-11 17:12:50 +0200

    update dev settings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,29 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
-    es6: true,
   },
   parserOptions: {
     parser: 'babel-eslint',
+    sourceType: 'module',
   },
-  extends: ['eslint:recommended', 'plugin:vue/recommended', 'plugin:prettier/recommended'],
-  plugins: ['vue', 'prettier'],
+  extends: ['airbnb-base', 'plugin:vue/recommended', 'prettier/vue', 'plugin:prettier/recommended'],
   rules: {
-    'vue/html-self-closing': [
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    'vue/component-name-in-template-casing': [
       'error',
+      'kebab-case',
       {
-        html: {
-          void: 'always',
-          normal: 'never',
-          component: 'any',
-        },
-        svg: 'always',
-        math: 'always',
+        ignores: [],
       },
     ],
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'prettier/prettier': ['error'],
+    'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
   },
 }
