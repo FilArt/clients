8614ac55e061b25fa95ce1b59c414ece644a0975
FilArt <artfil@protonmail.com>
2020-09-11 17:12:27 +0200

    update dependencies

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -43,6 +43,7 @@ export default {
     { src: '~/plugins/vue-mask', mode: 'client' },
     { src: '~/plugins/vue-beautiful-chat', mode: 'client' },
     { src: '~/plugins/localStorage', ssr: false, mode: 'client' },
+    { src: '~/plugins/range-date-picker', mode: 'client' },
   ],
   /*
    ** Nuxt.js dev-modules
