7d288d2398b7d09d1f11fb387fc0f295d88359b9
Filart <artfil@protonmail.com>
2020-09-22 16:07:26 +0200

    fix

--- apps/bids/permissions.py
+++ apps/bids/permissions.py
@@ -5,7 +5,7 @@ from apps.bids.models import Bid
 
 class BidsPermission(BasePermission):
     def has_permission(self, request, view):
-        return "bids" in request.user.permissions or request.user.role == "admin"
+        return "bids" in request.user.permissions or request.user.role in ("admin", "tramitacion", "agent")
 
     def has_object_permission(self, request, view, obj: Bid):
         requester = request.user
