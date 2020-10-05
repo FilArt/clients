a33940566ca73e5d2744d080e82cf1ca66422d23
FilArt <artfil@protonmail.com>
2020-10-05 23:50:09 +0200

    update vuex module

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -6,7 +6,10 @@ export const state = () => ({
   cities: [],
   privacyAccepted: false,
   companies: [],
+  tarifs: [],
   responsibles: [],
+  names: [],
+  bidToChange: null,
 })
 
 export const actions = {
@@ -14,9 +17,31 @@ export const actions = {
     const users = (await this.$axios.$get('users/users/?role=agent&fields=id,fullname&itemsPerPage=100')).results
     commit('setResponsibles', users)
   },
+  async fetchCompanies({ commit }) {
+    const companies = await this.$axios.$get('/calculator/companies/')
+    commit('setCompanies', companies)
+  },
+  async fetchTarifs({ commit }) {
+    const tarifs = await this.$axios.$get('/calculator/tarifs/')
+    commit('setTarifs', tarifs)
+  },
+  async fetchNames({ commit }) {
+    const names = (await this.$axios.$get('/calculator/offers/?fields=name')).map((item) => item.name)
+    commit('setNames', names)
+    console.log(names)
+  },
 }
 
 export const mutations = {
+  setNames(state, names) {
+    state.names = names
+  },
+  setTarifs(state, tarifs) {
+    state.tarifs = tarifs
+  },
+  setBidToChange(state, bid) {
+    state.bidToChange = bid
+  },
   setResponsibles(state, users) {
     state.responsibles = users
   },
