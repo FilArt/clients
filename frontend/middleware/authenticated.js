da63b9a661a193820dc072bf71096fa50218c389
FilArt <artfil@protonmail.com>
2020-11-13 13:28:55 +0100

    calc

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -1,5 +1,5 @@
 export default function ({ $auth, redirect, route }) {
-  if (!$auth.loggedIn && !route.name.includes('login')) {
+  if (!$auth.loggedIn && !route.name.includes('login') && !route.name.includes('calc')) {
     return redirect('/login')
   }
   if (route.name && route.name.startsWith('admin') && !['admin', 'support'].includes($auth.user.role)) {
