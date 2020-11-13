c60e3aafdb0954652169a4c608e52faf21509658
FilArt <artfil@protonmail.com>
2020-11-13 13:35:51 +0100

    Revert "unfix"
    This reverts commit bfb042f2

---
+++ frontend/pages/calc.vue
@@ -0,0 +1,13 @@
+<template>
+  <calculator />
+</template>
+
+<script>
+import Calculator from '@/components/forms/Calculator'
+export default {
+  components: { Calculator },
+  // layout: null,
+  auth: 'guest',
+  // middleware: null,
+}
+</script>
