1eb0006d1791384ed938f09858a1be5ab04d2714
FilArt <artfil@protonmail.com>
2020-11-19 13:06:44 +0100

    add status

--- frontend/pages/agente/agentes/_id.vue
+++ frontend/pages/agente/agentes/_id.vue
@@ -61,6 +61,7 @@ export default {
     const user = await $axios.$get(apiUrl)
     const statuses = [
       constants.statuses.KO,
+      constants.statuses.TRAMITACION_EN_PROCESSO,
       constants.statuses.PENDIENTE_TRAMITACION,
       constants.statuses.PAGADO,
       constants.statuses.PENDIENTE_PAGO,
