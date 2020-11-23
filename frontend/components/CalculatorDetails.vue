ba53cfb65da4315a87f6e4b4bf8658acc6beabc5
FilArt <artfil@protonmail.com>
2020-11-23 13:32:12 +0100

    fix noty about sending email

--- frontend/components/CalculatorDetails.vue
+++ frontend/components/CalculatorDetails.vue
@@ -2,7 +2,7 @@
   <v-card elevation="0">
     <v-card-actions>
       <v-spacer />
-      <v-btn style="margin-right: 7em" color="success" :loading="loading" rounded @click="getDetails">
+      <v-btn style="margin-right: 7em" color="success" :disabled="loading" rounded @click="getDetails">
         <v-icon>mdi-refresh</v-icon>
       </v-btn>
       <v-btn color="warning" :loading="loading" rounded x-large @click="send">
@@ -10,70 +10,74 @@
       </v-btn>
     </v-card-actions>
 
-    <v-card-text>
-      <v-row>
-        <v-col>
-          <template>
-            <v-text-field v-model="agent.fullname" label="Tu nombre" />
-            <email-field v-model="agent.email" label="Tu email" />
-            <phone-field v-model="agent.phone" />
-          </template>
+    <v-card-text v-if="sendingEmail"> </v-card-text>
 
-          <v-text-field v-model="clientName" label="Nombre de cliente" />
-          <email-field v-model="emailTo" label="Email de cliente" />
-        </v-col>
-        <v-col>
-          <v-text-field v-model="offerName" dense label="Oferta" />
-          <v-text-field v-model="direccion" dense label="Direccion" />
-          <v-text-field v-model="cups" dense label="CUPS" />
-          <v-text-field v-model="p1_offer" dense :label="`Precio por ${word} ${isGas ? '' : 'P1'}`" type="number" />
-          <v-text-field
-            v-show="showInput('p', 2)"
-            v-model="p2_offer"
-            dense
-            :label="`Precio por ${word} ${isGas ? '' : 'P2'}`"
-            type="number"
-          />
-          <v-text-field
-            v-show="showInput('p', 3)"
-            v-model="p3_offer"
-            dense
-            :label="`Precio por ${word} ${isGas ? '' : 'P3'}`"
-            type="number"
-          />
-          <v-text-field v-model="c1_offer" :label="`Precio por consumo ${isGas ? '' : 'P1'}`" type="number" dense />
-          <v-text-field
-            v-show="showInput('c', 2)"
-            v-model="c2_offer"
-            dense
-            :label="`Precio por consumo ${isGas ? '' : 'P2'}`"
-            type="number"
-          />
-          <v-text-field
-            v-show="showInput('c', 3)"
-            v-model="c3_offer"
-            dense
-            :label="`Precio por consumo ${isGas ? '' : 'P3'}`"
-            type="number"
-          />
-          <v-text-field v-model="otros" label="Otros" type="number" />
-          <v-text-field v-model="descuento" label="Descuento" suffix="%" type="number" />
+    <div v-else>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <template>
+              <v-text-field v-model="agent.fullname" label="Tu nombre" />
+              <email-field v-model="agent.email" label="Tu email" />
+              <phone-field v-model="agent.phone" />
+            </template>
 
-          <v-text-field v-model="rental" label="Alquiler de equipo" type="number" dense />
-          <v-text-field v-model="tax" label="Imp.Electricidad" type="number" dense />
-          <v-text-field v-model="iva" :label="form.igic ? 'IGIC' : 'IVA'" suffix="%" type="number" dense />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-textarea v-model="note" label="Nota" dense />
-        </v-col>
-      </v-row>
-    </v-card-text>
+            <v-text-field v-model="clientName" label="Nombre de cliente" />
+            <email-field v-model="emailTo" label="Email de cliente" />
+          </v-col>
+          <v-col>
+            <v-text-field v-model="offerName" dense label="Oferta" />
+            <v-text-field v-model="direccion" dense label="Direccion" />
+            <v-text-field v-model="cups" dense label="CUPS" />
+            <v-text-field v-model="p1_offer" dense :label="`Precio por ${word} ${isGas ? '' : 'P1'}`" type="number" />
+            <v-text-field
+              v-show="showInput('p', 2)"
+              v-model="p2_offer"
+              dense
+              :label="`Precio por ${word} ${isGas ? '' : 'P2'}`"
+              type="number"
+            />
+            <v-text-field
+              v-show="showInput('p', 3)"
+              v-model="p3_offer"
+              dense
+              :label="`Precio por ${word} ${isGas ? '' : 'P3'}`"
+              type="number"
+            />
+            <v-text-field v-model="c1_offer" :label="`Precio por consumo ${isGas ? '' : 'P1'}`" type="number" dense />
+            <v-text-field
+              v-show="showInput('c', 2)"
+              v-model="c2_offer"
+              dense
+              :label="`Precio por consumo ${isGas ? '' : 'P2'}`"
+              type="number"
+            />
+            <v-text-field
+              v-show="showInput('c', 3)"
+              v-model="c3_offer"
+              dense
+              :label="`Precio por consumo ${isGas ? '' : 'P3'}`"
+              type="number"
+            />
+            <v-text-field v-model="otros" label="Otros" type="number" />
+            <v-text-field v-model="descuento" label="Descuento" suffix="%" type="number" />
 
-    <v-card-text>
-      <v-sheet light v-html="htmlDetails" />
-    </v-card-text>
+            <v-text-field v-model="rental" label="Alquiler de equipo" type="number" dense />
+            <v-text-field v-model="tax" label="Imp.Electricidad" type="number" dense />
+            <v-text-field v-model="iva" :label="form.igic ? 'IGIC' : 'IVA'" suffix="%" type="number" dense />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea v-model="note" label="Nota" dense />
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-card-text>
+        <v-sheet light v-html="htmlDetails" />
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 
@@ -100,6 +104,7 @@ export default {
         ? JSON.parse(agent)
         : { fullname: user.fullname, email: user.email, phone: user.phone }
     return {
+      sendingEmail: false,
       loading: false,
       htmlDetails: null,
       direccion: null,
@@ -160,15 +165,17 @@ export default {
         await this.$swal({ title: 'Entrar correo', icon: 'error' })
         return
       }
+      this.sendingEmail = true
+      this.$vuetify.goTo(0)
       this.loading = true
       const { params } = this.getDataAndParams()
       try {
         await this.$axios.$get(`calculator/new_offer/?send=true&${params}`)
-        await this.$swal({ title: 'Sent...', icon: 'success' })
       } catch (e) {
         await this.$swal({ title: 'Not sent!', icon: 'error', text: e.response.data })
       } finally {
         this.loading = false
+        this.sendingEmail = false
       }
     },
     getDataAndParams() {
