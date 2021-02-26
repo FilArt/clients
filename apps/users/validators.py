0dc78e8110392ea05de6c10220214dfd805acab7
FilArt <artfil@protonmail.com>
2021-02-26 14:57:16 +0100

    cups checks

--- apps/users/validators.py
+++ apps/users/validators.py
@@ -4,3 +4,7 @@ from django.core.exceptions import ValidationError
 def cups_validator(value):
     if value.lower().endswith("0f"):
         raise ValidationError("CUPS contiene un error: `0f` al final")
+    if not str(value).startswith('ES'):
+        raise ValidationError(f"cups not starting with ES: {value}")
+    if len(str(value)) != 20:
+        raise ValidationError(f"cups length not equal 20: {value}")
