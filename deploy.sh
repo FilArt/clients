19aa5bfa415ae38e0eeb7683ae1e622195b7c560
FilArt <artfil@protonmail.com>
2021-03-23 15:35:05 +0100

    lint

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ set -e
 cd frontend
 
 echo Input backend host
-read BACKEND_HOST
+read -r BACKEND_HOST
 echo Input server alias
-read SERVER
+read -r SERVER
 
 BACKEND_HOST=$BACKEND_HOST NODE_ENV=production yarn run generate
 
 cd ..
 
-rsync -rvzz --delete -e ssh frontend/dist/ $SERVER:/var/django/clients/static
+rsync -rvzz --delete -e ssh frontend/dist/ "$SERVER":/var/django/clients/static
 
 exec ./backend_deploy.sh
