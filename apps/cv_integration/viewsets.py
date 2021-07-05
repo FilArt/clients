33b42eec486e4234e10c2296b7ce311f4f95a33c
FilArt <artfil@protonmail.com>
2021-07-05 16:16:12 +0200

    fiiix

--- apps/cv_integration/viewsets.py
+++ apps/cv_integration/viewsets.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import requests
@@ -51,41 +52,45 @@ class CallVisitUserViewSet(viewsets.ModelViewSet):
             bids = getattr(client, "bids").all()
             for bid in bids:
                 punto: Punto = bid.punto
-                item = {k: v for k, v in {
-                    "name": client.fullname,
-                    "postalcode": punto.postalcode,
-                    "cups": punto.cups_luz or punto.cups_gas,
-                    "cups_gas": punto.cups_gas,
-                    "tarif": punto.tarif_luz,
-                    "tarif_gas": punto.tarif_gas,
-                    "client_type": "J" if punto.category == "business" else "F",
-                    "persona_contacto": punto.legal_representative or client.legal_representative,
-                    "commers": punto.company_luz.name if punto.company_luz else None,
-                    "province": punto.province,
-                    "poblacion": punto.locality,
-                    "direccion": punto.address,
-                    "fecha_firma": process_date(bid.fecha_firma),
-                    "fecha_cambio": process_date(punto.last_time_company_luz_changed),
-                    "email": client.email,
-                    "oferta": bid.offer.name,
-                    "p1": punto.p1,
-                    "p2": punto.p2,
-                    "p3": punto.p3,
-                    "p4": punto.p4,
-                    "p5": punto.p5,
-                    "p6": punto.p6,
-                    "consumo": punto.consumo_annual_luz,
-                    "consumo_gas": punto.consumo_annual_gas,
-                    "operator": request.data.get("operator"),
-                    "manager": request.data.get("manager"),
-                    "status": request.data.get("status"),
-                    "tele": request.data.get("tele"),
-                    "dni": punto.dni,
-                    "cif": client.cif_nif,
-                    "iban": punto.iban,
-                    "phones2": [p for p in [client.phone, client.phone_city] if p],
-                    "is_client": True,
-                }.items() if v}
+                item = {
+                    k: v
+                    for k, v in {
+                        "name": client.fullname,
+                        "postalcode": punto.postalcode,
+                        "cups": punto.cups_luz or punto.cups_gas,
+                        "cups_gas": punto.cups_gas,
+                        "tarif": punto.tarif_luz,
+                        "tarif_gas": punto.tarif_gas,
+                        "client_type": "J" if punto.category == "business" else "F",
+                        "persona_contacto": punto.legal_representative or client.legal_representative,
+                        "commers": punto.company_luz.name if punto.company_luz else None,
+                        "province": punto.province,
+                        "poblacion": punto.locality,
+                        "direccion": punto.address,
+                        "fecha_firma": process_date(bid.fecha_firma),
+                        "fecha_cambio": process_date(punto.last_time_company_luz_changed),
+                        "email": client.email,
+                        "oferta": bid.offer.name,
+                        "p1": punto.p1,
+                        "p2": punto.p2,
+                        "p3": punto.p3,
+                        "p4": punto.p4,
+                        "p5": punto.p5,
+                        "p6": punto.p6,
+                        "consumo": punto.consumo_annual_luz,
+                        "consumo_gas": punto.consumo_annual_gas,
+                        "operator": request.data.get("operator"),
+                        "manager": request.data.get("manager"),
+                        "status": request.data.get("status"),
+                        "tele": request.data.get("tele"),
+                        "dni": punto.dni,
+                        "cif": client.cif_nif,
+                        "iban": punto.iban,
+                        "phones2": [p for p in [client.phone, client.phone_city] if p],
+                        "is_client": True,
+                    }.items()
+                    if v
+                }
                 response = authed_cv_client.post(f"{settings.CALL_VISIT_URL}/api/cards/", json=item)
                 if not response.ok and (
                     "La tarjeta con ese cups ya existe" in response.text or "Multipunto" in response.text
@@ -97,7 +102,10 @@ class CallVisitUserViewSet(viewsets.ModelViewSet):
                     response = authed_cv_client.patch(f"{settings.CALL_VISIT_URL}/api/cards/{card_id}/", json=item)
 
                 if not response.ok:
-                    errors.append({client.id: response.json()})
+                    try:
+                        errors.append({client.id: response.json()})
+                    except json.JSONDecodeError:
+                        errors.append({client.id: response.text})
 
         if errors:
             raise ValidationError(errors)
