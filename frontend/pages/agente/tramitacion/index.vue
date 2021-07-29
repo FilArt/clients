a98a3e4c4282b61a91dafdd68c9b80084d24e5c5
FilArt <artfil@protonmail.com>
2021-07-29 17:30:28 +0200

    fix filter

--- frontend/pages/agente/tramitacion/index.vue
+++ frontend/pages/agente/tramitacion/index.vue
@@ -1,13 +1,21 @@
 <template>
   <v-card>
     <v-card-text>
-      <users-table-expandable :headers="headers" mode="tramitacion" is-support show-date-filters use-full-name />
+      <users-table-expandable
+        :headers="headers"
+        :statuses="statuses"
+        mode="tramitacion"
+        is-support
+        show-date-filters
+        use-full-name
+      />
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import UsersTableExpandable from '~/components/tables/UsersTableExpandable'
+import constants from '../../../lib/constants'
 
 export default {
   components: {
@@ -16,6 +24,7 @@ export default {
   data() {
     return {
       headers: ['id', 'fecha_registro', 'fullname', 'bids_count'],
+      statuses: Object.values(constants.statuses.tramitacion),
     }
   },
 }
