2c60e5c0b20443fbd0ee0d21c421ed3adbba63ec
FilArt <artfil@protonmail.com>
2021-10-06 12:49:14 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,48 @@
 
 set -e
 
+f=0 b=0 s= h=
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -f|--frontend) f=1 ;;
+        -b|--backend) b=1 ;;
+        -s) s="$2"; shift ;;
+        -h) h="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -z "$s" ]
+then
+echo 'server name not provided'
+exit 1
+fi
+
+if [ -z "$h" ]
+then
+echo 'host not provided'
+exit 1
+fi
+
+echo start deploy to server $s with host $h
+
+if [ $f == 1 ]
+then
+echo deploying frontend
 cd frontend
-
-echo Input backend host
-read -r BACKEND_HOST
-echo Input server alias
-read -r SERVER
-
-BACKEND_HOST=$BACKEND_HOST NODE_ENV=production yarn run generate
-
+BACKEND_HOST=$h NODE_ENV=production yarn run generate
 cd ..
+rsync -rvzz --delete -e ssh frontend/dist/ --exclude 'media' "$s":/var/django/clients/static
+fi
 
-rsync -rvzz --delete -e ssh frontend/dist/ "$SERVER":/var/django/clients/static
+if [ $b == 1 ]
+then
+echo deploying backend
 
-ssh "$SERVER" "\
+ssh "$s" "\
+  source .zprofile; \
   cd /srv/http/clients; \
   git pull; \
   source .venv/bin/activate; \
@@ -24,3 +52,4 @@ ssh "$SERVER" "\
   ./manage.py compilemessages -l es &> /dev/null; \
   ./manage.py collectstatic --noinput; \
    supervisorctl restart clients:"
+fi
